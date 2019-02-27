1a665107c6da09284553231b7b70566b4a602f03
Zonlgin-Li6565 <development.my6565@gmail.com>
2019-02-26 21:35:12 -0600

    Adding more different formats for movie

--- scraper/spider/movie_parser.py
+++ scraper/spider/movie_parser.py
@@ -29,13 +29,17 @@ class MovieParser:
             # TODO: Log
             pass
 
-        grossing_pattern = re.compile('.*\\$([0-9]+(\\.[0-9]+)?) million')
+        grossing_pattern = re.compile('\\$([1-9][0-9]*(\\.[0-9]+)?) million')
+        grossing_pattern_2 = re.compile(
+            '\\$(([1-9]|[1-9][0-9]|[1-9][0-9][0-9])(,[0-9][0-9][0-9])*)\D')
         if 'Box office' in infobox:
             tag = infobox['Box office']
             tag_str = str(tag).replace(u'\xa0', u' ')
-            matched_grossing = grossing_pattern.search(tag_str)
+            matched_grossing = (grossing_pattern.search(tag_str)
+                                or grossing_pattern_2.search(tag_str))
+            print(matched_grossing.group(1))
             if matched_grossing:
-                grossing = float(matched_grossing.group(1))
+                grossing = float(matched_grossing.group(1).replace(',', ''))
                 movie.total_grossing = grossing
             # TODO: Log
         else:
@@ -51,6 +55,34 @@ class MovieParser:
             for link in infobox['Starring'].find_all(
                     'a', href=re.compile('/wiki/')):
                 urls.append(link.href)
+        else:
+            # TODO: Log
+            pass
+
+        return urls
+
+    @staticmethod
+    def parse_cast(html: Tag) -> List[Url]:
+        urls = []
+        cast_h2 = html.find_all(
+            lambda tag: tag.name == 'h2' and tag.find_all('span', id='Cast'))
+        if len(cast_h2) != 1:
+            # TODO: Log
+            return urls
+        cast_list = None
+        for sibling in cast_h2[0].next_siblings:
+            if hasattr(sibling, 'name') and sibling.name == 'ul':
+                cast_list = sibling
+                break
+            elif (hasattr(sibling, 'name') and sibling.name == 'div'
+                  and sibling.ul is not None):
+                cast_list = sibling.ul
+                break
+        if cast_list:
+            for li in cast_list.find_all('li'):
+                link = li.find('a', href=re.compile('/wiki/'))
+                if link:
+                    urls.append(link.href)
         else:
             # TODO: Log
             pass
--- test/parse_movie_test.py
+++ test/parse_movie_test.py
@@ -13,7 +13,8 @@ from scraper.spider.utils import parse_infobox
                            'How to Train Your Dragon: The Hidden World', 2019,
                            274.9),
                           ('movie2.html', 'The Wandering Earth', 2019, 650),
-                          ('movie4.html', 'JFK', 1991, 205.4)])
+                          ('movie4.html', 'JFK', 1991, 205.4),
+                          ('movie5.html', 'Making Mr. Right', 1987, 1584970)])
 def test_movie_parser(file_name, name, year, grossing):
     with open(os.path.join('test/test_files', file_name)) as html:
         movie_parser = MovieParser()
@@ -40,3 +41,17 @@ def test_movie_starring(file_name, num_starring):
         infobox_dict = parse_infobox(infobox)
         stars = movie_parser.parse_staring(infobox_dict)
         assert num_starring == len(stars)
+
+
+@pytest.mark.parametrize('file_name, num_casting',
+                         [('movie3.html', 10),
+                          ('movie1.html', 16),
+                          ('movie2.html', 5),
+                          ('movie4.html', 34),
+                          ('movie5.html', 0)])
+def test_movie_starring(file_name, num_casting):
+    with open(os.path.join('test/test_files', file_name)) as html:
+        movie_parser = MovieParser()
+        soup = BeautifulSoup(html, 'html.parser')
+        casts = movie_parser.parse_cast(soup.html)
+        assert num_casting == len(casts)
